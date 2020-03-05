a3af3c794f87c099eac092f1b27b39e98b8c854d
Sebastian-lopez1 <sebastian_lopez1@baylor.edu>
2020-03-05 09:50:26 -0600

    Lab07

--- Lab07/Lab07.runs/synth_1/runme.sh
+++ Lab07/Lab07.runs/synth_1/runme.sh
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='C:/Users/LOPEZS/Desktop/Lab07/Lab07.runs/synth_1'
+HD_PWD='C:/Users/LOPEZS/Desktop/DL2010_Lopez/Lab07/Lab07.runs/synth_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
--- Lab07/Lab07.runs/synth_1/sseg1_BCD.tcl
+++ Lab07/Lab07.runs/synth_1/sseg1_BCD.tcl
@@ -17,24 +17,25 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
+set_param xicom.use_bs_reader 1
 create_project -in_memory -part xc7a35tcpg236-1
 
 set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
-set_property webtalk.parent_dir C:/Users/LOPEZS/Desktop/Lab07/Lab07.cache/wt [current_project]
-set_property parent.project_path C:/Users/LOPEZS/Desktop/Lab07/Lab07.xpr [current_project]
+set_property webtalk.parent_dir C:/Users/LOPEZS/Desktop/DL2010_Lopez/Lab07/Lab07.cache/wt [current_project]
+set_property parent.project_path C:/Users/LOPEZS/Desktop/DL2010_Lopez/Lab07/Lab07.xpr [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
 set_property board_part digilentinc.com:basys3:part0:1.1 [current_project]
-set_property ip_output_repo c:/Users/LOPEZS/Desktop/Lab07/Lab07.cache/ip [current_project]
+set_property ip_output_repo c:/Users/LOPEZS/Desktop/DL2010_Lopez/Lab07/Lab07.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 read_verilog -library xil_defaultlib -sv {
-  C:/Users/LOPEZS/Desktop/Lab07/Lab07.srcs/sources_1/new/BCD11.sv
-  C:/Users/LOPEZS/Desktop/Lab07/Lab07.srcs/sources_1/new/add3.sv
-  C:/Users/LOPEZS/Desktop/Lab07/Lab07.srcs/sources_1/new/mux2_4b.sv
-  C:/Users/LOPEZS/Desktop/Lab07/Lab07.srcs/sources_1/new/sseg_decoder.sv
-  C:/Users/LOPEZS/Desktop/Lab07/Lab07.srcs/sources_1/new/sseg1_BCD.sv
+  C:/Users/LOPEZS/Desktop/DL2010_Lopez/Lab07/Lab07.srcs/sources_1/new/BCD11.sv
+  C:/Users/LOPEZS/Desktop/DL2010_Lopez/Lab07/Lab07.srcs/sources_1/new/add3.sv
+  C:/Users/LOPEZS/Desktop/DL2010_Lopez/Lab07/Lab07.srcs/sources_1/new/mux2_4b.sv
+  C:/Users/LOPEZS/Desktop/DL2010_Lopez/Lab07/Lab07.srcs/sources_1/new/sseg_decoder.sv
+  C:/Users/LOPEZS/Desktop/DL2010_Lopez/Lab07/Lab07.srcs/sources_1/new/sseg1_BCD.sv
 }
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
@@ -44,11 +45,11 @@ read_verilog -library xil_defaultlib -sv {
 foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
-read_xdc C:/Users/LOPEZS/Downloads/sseg.xdc
-set_property used_in_implementation false [get_files C:/Users/LOPEZS/Downloads/sseg.xdc]
+read_xdc C:/Users/LOPEZS/Desktop/DL2010_Lopez/Lab07/sseg.xdc
+set_property used_in_implementation false [get_files C:/Users/LOPEZS/Desktop/DL2010_Lopez/Lab07/sseg.xdc]
 
-read_xdc C:/Users/LOPEZS/Downloads/switches.xdc
-set_property used_in_implementation false [get_files C:/Users/LOPEZS/Downloads/switches.xdc]
+read_xdc C:/Users/LOPEZS/Desktop/DL2010_Lopez/Lab07/switches.xdc
+set_property used_in_implementation false [get_files C:/Users/LOPEZS/Desktop/DL2010_Lopez/Lab07/switches.xdc]
 
 set_param ips.enableIPCacheLiteLoad 1
 close [open __synthesis_is_running__ w]
